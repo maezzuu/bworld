f54db19701681b78b4ad4a3c1f2ee3e528e5baa9
maezzuu <o3o_hell0@naver.com>
2024-10-15 14:52:30 +0900

    bworld스토리

---
+++ js/common.js
@@ -0,0 +1,70 @@
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".suggestSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1280: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    600: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+  },
+});
+
+
+var swiper = new Swiper(".hotnewSwiper", {
+  slidesPerView: 'auto',
+  spaceBetween: 2,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // 모바일 설정
+    768: {
+        slidesPerView: 2, // 한 줄에 2개 보이게 설정
+        spaceBetween: 2,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      
+      
+    },
+  },
+});
+
+var swiper = new Swiper(".serviceSwiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
